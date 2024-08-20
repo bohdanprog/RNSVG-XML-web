9f3d9d1c9863de0f6f00ea784207b71258dcb966
Matheus Ricardo dos Santos <matheus@MacBook-Air-de-Matheus.local>
2024-08-20 14:47:00 -0300

    fix project setup and add SvgXml usage sample

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import { SvgXml } from 'react-native-svg'
 const App = () => {
-  return <h1>Title</h1>;
+  return <SvgXml xml={`<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+  <circle r="45" cx="50" cy="50" stroke="green" stroke-width="3" fill="red" />
+</svg> `}></SvgXml>;
 };
 
 export default App;
